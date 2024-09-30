c527bddbf9e593c1273a26652bbd9c235dc3259c
Thomas McLaughlan <tmclaughlan@scottlogic.com>
2024-09-30 14:55:11 +0100

    Add tailwind, work in progress

---
+++ ui/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      spacing: {
+        'content': '64rem'
+      }
+    },
+    colors: {
+      transparent: "transparent",
+      'primary': "#38023b",
+      'secondary': "#a288e3",
+      'tertiary': "#bbd5ed",
+      'accent-red': "#fb3640",
+      'accent-yellow': "#fde74c",
+      'accent-green': "#69fc96",
+      'white': "#fdeefd",
+      'black': "#110111",
+      'background': "#fdebfe"
+    }
+  },
+  plugins: [],
+}
+
