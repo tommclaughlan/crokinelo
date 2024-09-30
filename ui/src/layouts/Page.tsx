7a8b3d2914b241a0e935852305a26149a5d1c0e1
Thomas McLaughlan <tmclaughlan@scottlogic.com>
2024-09-30 14:55:11 +0100

    Add tailwind, work in progress

--- ui/src/layouts/Page.tsx
+++ ui/src/layouts/Page.tsx
@@ -11,19 +11,17 @@ interface PageProps {
 
 function Page({ children }: PageProps) {
     return (
-        <>
-            <div className="hero is-small is-primary">
-                <div className="hero-body has-text-centered">
-                    <p className="title">{randomQuote}</p>
-                    <p className="subtitle">
-                        Puttin' croks in 'oles since 2024
-                    </p>
-                </div>
+        <div className="flex flex-col items-center">
+            <div className="bg-primary text-white w-full text-center p-6">
+                <p className="text-3xl font-semibold">{randomQuote}</p>
+                <p className="text-xl opacity-65">
+                    Puttin' croks in 'oles since 2024
+                </p>
             </div>
-            <div className="container page-body is-flex is-flex-direction-column">
+            <div className="flex flex-col w-content">
                 {children}
             </div>
-        </>
+        </div>
     );
 }
 
